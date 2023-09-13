dbe001a1e4f4ffa4da6222d069b29a003fe0ce9c
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-13 02:24:29 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000;
 const mongoose = require('mongoose');
+const port = 5000;
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('Database connected')).catch((err) => console.log(err));
 
