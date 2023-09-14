7fe1dc2235dab6bc8d545bcd45334313d2fb7c2a
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-14 04:04:14 +0100

    update

---
+++ routes/models/Products.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+
+    title: { type: String, required: true },
+    supplier: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    description: { type: String, required: true },
+    product_location: { type: String, required: true },
+
+}, { timestamps: true })
+
+module.export = module.model("Product", ProductSchema);
