aa61db593959112f52aa78e03d6244ede8f32d23
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-16 00:48:37 +0100

    updating my controllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,7 +2,7 @@ const Product = require('../models/Products');
 
 module.exports = {
 
-    createProduct: async(req, res) => {
+    createProduct: async (req, res) => {
         const newProduct = new Product(req.body);
         try {
             await newProduct.save();
@@ -10,6 +10,24 @@ module.exports = {
         } catch (err) {
             res.status(500).json("Failed to create Product")
         }
+    },
+
+    getAllProduct: async (req, res) => {
+        try {
+            const products = await Product.find().sort({ createAt: -1 });
+            res.send(200).json(products)
+        } catch (error) {
+            res.status(500).json("Failed to get Product")
+        }
+    },
+
+    getProduct: async (req, res) => {
+        try {
+            const product = await Product.findById(req.params.id)
+            res.status(200).json(product)
+        } catch (error) {
+            res.status(500).json("Failed to get Product")
+        }
     }
-    
+
 }
