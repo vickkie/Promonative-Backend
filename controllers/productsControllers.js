8d719d626ef5883b198e7983bf61ad67099966a5
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-17 00:42:00 +0100

    update

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -8,7 +8,7 @@ module.exports = {
             await newProduct.save();
             res.status(200).json('Product created successfully')
         } catch (err) {
-            res.status(500).json("Failed to create Product")
+            res.status(500).json("Failed to create product")
         }
     },
 
@@ -17,7 +17,7 @@ module.exports = {
             const products = await Product.find().sort({ createAt: -1 });
             res.send(200).json(products)
         } catch (error) {
-            res.status(500).json("Failed to get Product")
+            res.status(500).json("Failed to get product")
         }
     },
 
@@ -26,8 +26,31 @@ module.exports = {
             const product = await Product.findById(req.params.id)
             res.status(200).json(product)
         } catch (error) {
-            res.status(500).json("Failed to get Product")
+            res.status(500).json("Failed to get product")
+        }
+    },
+
+    searchProducts: async (req, res) => {
+        try {
+            const result = await Product.aggregate(
+                [
+                    {
+                        $search: {
+                            index: "furniture",
+                            text: {
+                                query: req.params.key,
+                                path: {
+                                    wildcard: "*"
+                                }
+                            }
+                        }
+                    }
+                ]
+            )
+
+            res.status(200).json(result)
+        } catch (error) {
+            res.status(500).json("Failed to get products")
         }
     }
-
 }
