8ada4cf02c7d0bd085cdfa8a75e82b926ad4e2f8
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-10-11 03:11:25 +0100

    auth controlls

--- models/Other.js
+++ models/Other.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
 
+    userId: { type: String, required: true },
+    customerId: { type: String, required: true },
+    customerId: { type: String, required: true },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: { type: Number, required: true },
+    subTotal: { type: Number, required: true },
+    total: { type: Number, required: true },
+    delivery_status: { type: Number, default: "Pending" },
+    payment_status: { type: String, required: true },
+
 }, { timestamps: true })
 
 module.exports = mongoose.model("Order", OrderSchema);
