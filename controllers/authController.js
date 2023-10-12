488f2c7a28dbc1762f79c225134142ccfa73c7be
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-10-12 03:38:11 +0100

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,46 @@
 const User = require('../models/User');
-const cryptoJs = require('crypto-js')
+const CryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+
+    createUser: async (req, res) => {
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            location: req.body.location,
+            password: CryptoJs.AES.encrypt(req.body.password, process.env.SECRET).toString(),
+
+        });
+
+        try {
+            await newUser.save();
+            res.status(201).json({ message: "User successfully created" });
+        } catch (error) {
+            res.status(500).json({ message: error });
+        }
+    },
+
+    loginUser: async (req, res) => { 
+        try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status.json({message: "Wrong credentials, provide a valid user email"});
+        const decryptedPassword = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
+        const decryptedPass = decryptedPassword.toString(CryptoJs.enc.Utf8);
+
+        decryptedPass !== req.body.password && res.status(401).json("Your password is incorrect");
+
+         const userToken = jwt.sign({
+            id: user.id
+         }, process.env.JWT_SEC, {expiresIn: "7d"} );
+
+         const {password, _v, createAt, updatedAt, ...userData} = user._doc;
+         res.status(200).json({...userData, token: userToken})
+
+        } catch (error) {
+            res.status(500).json({message: error});
+        }
+    },
+
+};
