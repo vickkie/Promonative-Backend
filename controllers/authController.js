bded4ac7444cf8bc80c4d77aeeed8ba5194a6a7a
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-12-01 09:55:33 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,31 +15,42 @@ module.exports = {
         });
 
         try {
-            await newUser.save();
-            res.status(201).json({ message: "User successfully created" });
+            await newUser.save()
+            res.status(201).json({ message: "A User successfully created" });
         } catch (error) {
             res.status(500).json({ message: error });
         }
     },
 
-    loginUser: async (req, res) => { 
+    loginUser: async (req, res) => {
+
         try {
-        const user = await User.findOne({email: req.body.email});
-        !user && res.status.json({message: "Wrong credentials, provide a valid user email"});
-        const decryptedPassword = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
-        const decryptedPass = decryptedPassword.toString(CryptoJs.enc.Utf8);
 
-        decryptedPass !== req.body.password && res.status(401).json("Your password is incorrect");
+            const user = await User.findOne({ email: req.body.email });
 
-         const userToken = jwt.sign({
-            id: user.id
-         }, process.env.JWT_SEC, {expiresIn: "7d"} );
+            if (!user) {
+                return res.status.json({ message: "Wrong credentials, provide a valid user email" });
+            }
 
-         const {password, _v, createAt, updatedAt, ...userData} = user._doc;
-         res.status(200).json({...userData, token: userToken})
+            const decryptedPassword = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
+            const decryptedPass = decryptedPassword.toString(CryptoJs.enc.Utf8);
+
+            if (decryptedPass !== req.body.password) {
+                return res.status(401).json("Your password is incorrect");
+            }
+
+
+
+
+            const userToken = jwt.sign({
+                id: user.id
+            }, process.env.JWT_SEC, { expiresIn: "7d" });
+
+            const { password, _v, createAt, updatedAt, ...userData } = user._doc;
+            res.status(200).json({ ...userData, token: userToken })
 
         } catch (error) {
-            res.status(500).json({message: error});
+            res.status(500).json({ message: error });
         }
     },
 
